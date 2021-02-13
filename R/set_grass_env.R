5395e07e425ddf34e13f3b33f743d1dd830a7e33
Hunter Gleason <Hunter.Gleason@gov.bc.ca>
2021-02-12 16:16:49 -0800

    Added plots to upstream basin statistics portion of vignette

--- R/set_grass_env.R
+++ R/set_grass_env.R
@@ -1,39 +1,29 @@
 #' Function initializes a temporary GRASS-GIS environment, sets the region
-#' parameters to match those of user provided DEM, writes covariate 
-#' raster to the GRASS environment and generates a accumulation and 
-#' drainage direction raster from DEM. 
+#' parameters to match those of user provided DEM, writes input
+#' raster to the GRASS environment. 
 #'
 #' This function relies on 'rgrass7' to initialize and populate a 
 #' temporary GRASS-GIS environment. The user must have GRASS-GIS
 #' binaries installed, and provide the path to these, e.g., "/usr/lib/grass78".
-#' In addition, the GRASS 'r.stream.extract' Add-On must be installed.
 #' User must provide a DEM, which will be used for subsequent processing,
 #' the GRASS environment will be set to match the DEM extent and CRS.
-#' A list of 'raster' objects representing covariate layers must be 
+#' A list of 'raster' objects representing input layers must be 
 #' provided, as each layer will be written to the GRASS environment. 
-#' For each covariate raster, a character vector of desired raster 
-#' layer names must be provided. Both a accumulation and drainage 
-#' direction raster are computed. A stream network is extracted,
-#' using a simple accumulation threshold. For systems with limited
-#' resources, the 'seg' parameter can be set to 'T" with a maximum 
-#' RAM memory MB defined by 'memory_mb'.
+#' For each input raster, a character vector of desired raster 
+#' layer names must be provided.
 #'
 #' @param gisBase The directory path to GRASS binaries and libraries, 
 #' containing bin and lib sub directories among others, see rgrass7::initGRASS
 #' @param DEM A digital elevation model as a 'raster' object with an 
 #' extent encompassing all pour points of interest. Note, all layers
 #' must match the CRS of this layer. 
-#' @param lyr_list A list of 'raster' objects representing the covariate 
+#' @param lyr_list A list of 'raster' objects representing the input 
 #' raster layers that will be written to the GRASS environment. 
 #' @param lyr_names A character vector corresponding to 'lyr_list' defining
-#' the names of each covarite raster layer. 
-#' @param acc_thresh The accumulation threshold to be passed to 'r.stream.extract'.
-#' @param seg Should data be segmented to disk to save RAM on resource limited systems,
-#' defaults to FALSE. 
-#' @param memory_mb Maximum memory in MB to allocate if 'seg' is equal to TRUE. 
+#' the names of each input raster layer. 
 #' @return NULL, only initializes and populates a GRASS-GIS environment. 
 #' @export
-set_grass_env<-function(gisbase,DEM,lyr_list,lyr_names,acc_thresh,seg=F,memory_mb=NULL)
+set_grass_env<-function(gisbase,DEM,lyr_list,lyr_names)
 {
   #Set up temporary GRASS Environment, must provide gisBase, e.g., gisBase="/usr/lib/grass78"#
   cat("Initializing GRASS ...")
@@ -56,6 +46,12 @@ set_grass_env<-function(gisbase,DEM,lyr_list,lyr_names,acc_thresh,seg=F,memory_m
   rgrass7::execGRASS('g.region',
                      parameters = list(raster='dem'))
   
+  rgrass7::execGRASS("g.proj", flags = c("c", "quiet"),
+            parameters = list(
+              georef = 'dem'
+            ))
+  
+  
   #Write each disturbance layer to GRASS environment 
   for(lyr in c(1:length(lyr_list)))
   {
@@ -66,55 +62,55 @@ set_grass_env<-function(gisbase,DEM,lyr_list,lyr_names,acc_thresh,seg=F,memory_m
                        overwrite = T)
   }
   
-  #Run r.watershed in GRASS to create DEM derivatives and stream network, 31 MB of RAM for 1 million cells 
-  if(seg==F)
-  {
-  cat("Extracting streams from DEM, generating GRASS derivatives ...")
-  rgrass7::execGRASS("r.watershed",
-                     parameters = list(elevation='dem',
-                                       threshold=2,
-                                       accumulation='acc',
-                                       tci='topo_idx',
-                                       spi='strm_pow',
-                                       drainage='dir',
-                                       stream='stream_r',
-                                       length_slope='slope_lngth',
-                                       slope_steepness='steepness'),
-                     flags = c('overwrite',
-                               'quiet',
-                               'a'))
-  
-  rgrass7::execGRASS("r.stream.extract",
-                     parameters = list(elevation='dem',
-                                       accumulation='acc',
-                                       threshold=acc_thresh,
-                                       stream_vector='stream_v'))
-  }else{
-    cat("Extracting streams from DEM, generating GRASS derivatives ...")
-    rgrass7::execGRASS("r.watershed",
-                       parameters = list(elevation='dem',
-                                         threshold=2,
-                                         accumulation='acc',
-                                         tci='topo_idx',
-                                         spi='strm_pow',
-                                         drainage='dir',
-                                         stream='stream_r',
-                                         length_slope='slope_lngth',
-                                         slope_steepness='steepness',
-                                         memory=memory_mb),
-                       flags = c('overwrite',
-                                 'quiet',
-                                 'a',
-                                 'm'))
-    
-    rgrass7::execGRASS("r.stream.extract",
-                       parameters = list(elevation='dem',
-                                         accumulation='acc',
-                                         threshold=acc_thresh,
-                                         stream_vector='stream_v',
-                                         memory=memory_mb),
-                       flags = c('overwrite',
-                                 'quiet'))
-  }
+  # #Run r.watershed in GRASS to create DEM derivatives and stream network, 31 MB of RAM for 1 million cells 
+  # if(seg==F)
+  # {
+  # cat("Extracting streams from DEM, generating GRASS derivatives ...")
+  # rgrass7::execGRASS("r.watershed",
+  #                    parameters = list(elevation='dem',
+  #                                      threshold=2,
+  #                                      accumulation='acc',
+  #                                      tci='topo_idx',
+  #                                      spi='strm_pow',
+  #                                      drainage='dir',
+  #                                      stream='stream_r',
+  #                                      length_slope='slope_lngth',
+  #                                      slope_steepness='steepness'),
+  #                    flags = c('overwrite',
+  #                              'quiet',
+  #                              'a'))
+  # 
+  # rgrass7::execGRASS("r.stream.extract",
+  #                    parameters = list(elevation='dem',
+  #                                      accumulation='acc',
+  #                                      threshold=acc_thresh,
+  #                                      stream_vector='stream_v'))
+  # }else{
+  #   cat("Extracting streams from DEM, generating GRASS derivatives ...")
+  #   rgrass7::execGRASS("r.watershed",
+  #                      parameters = list(elevation='dem',
+  #                                        threshold=2,
+  #                                        accumulation='acc',
+  #                                        tci='topo_idx',
+  #                                        spi='strm_pow',
+  #                                        drainage='dir',
+  #                                        stream='stream_r',
+  #                                        length_slope='slope_lngth',
+  #                                        slope_steepness='steepness',
+  #                                        memory=memory_mb),
+  #                      flags = c('overwrite',
+  #                                'quiet',
+  #                                'a',
+  #                                'm'))
+  #   
+  #   rgrass7::execGRASS("r.stream.extract",
+  #                      parameters = list(elevation='dem',
+  #                                        accumulation='acc',
+  #                                        threshold=acc_thresh,
+  #                                        stream_vector='stream_v',
+  #                                        memory=memory_mb),
+  #                      flags = c('overwrite',
+  #                                'quiet'))
+  # }
   cat("GRASS-GIS env initialized and populated ...")
 }
