0ec1fc6c736d2f44ebfc43a0be78db8d8a39a52d
Hunter Gleason <Hunter.Gleason@gov.bc.ca>
2021-03-24 11:29:19 -0700

    Decided to use gdal instead of sf for polyoginizing for speed reasons

--- R/raster_to_clean_polygon.R
+++ R/raster_to_clean_polygon.R
@@ -1,3 +1,64 @@
+#' @title gdal_polygonizeR
+#' @description R Wrapper for the gdal_polygonize.py python script (http://www.gdal.org/gdal_polygonize.html)
+#' This utility creates vector polygons for all connected regions of pixels in the raster sharing
+#' a common pixel value. Each polygon is created with an attribute indicating the pixel value of
+#' that polygon. Can be userful for example to create fishnet polygons (see "create_fishnet")
+#'
+#' @param x          `character`filename of a raster file, or "R" raster object
+#' @param outshape   `character`filename of the desired output polygon shapefile
+#' @param gdalformat  defaults to ESRI - don't change
+#' @param pypath     `character` path of python  - if `NULL` (the default) the script tries to
+#' automatically retrieve it
+#' @param quiet      `logical` if TRUE, limit the messages (defaults to TRUE)
+#' @param overwrite  `logical` If TRUE overwrite shapefile if already existing (defaults to FALSE)
+#' @param python3 `logical` If TRUE uses python3 instead of python2.x (defaults to TRUE)
+#' @return A sf polygon object.
+#' @export
+#' @importFrom raster writeRaster
+#' @importFrom methods is
+#' @author Original code by Jonh Baumgartner
+#' [https://johnbaumgartner.wordpress.com/2012/07/26/getting-rasters-into-shape-from-r/](), with
+#' slight modifications by L.Busetto & H.Gleason 
+gdal_polygonizeR <- function(x, outshape=NULL, gdalformat = 'ESRI Shapefile', 
+                             pypath=NULL, quiet=TRUE,python3=T) {
+  if (is.null(pypath)) {
+    pypath <- Sys.which('gdal_polygonize.py')
+  }
+  if (!file.exists(pypath)) stop("Can't find gdal_polygonize.py on your system.") 
+  owd <- getwd()
+  on.exit(setwd(owd))
+  setwd(dirname(pypath))
+  if (!is.null(outshape)) {
+    outshape <- sub('\\.shp$', '', outshape)
+    f.exists <- file.exists(paste(outshape, c('shp', 'shx', 'dbf'), sep='.'))
+    if (any(f.exists)) 
+      stop(sprintf('File already exists: %s', 
+                   toString(paste(outshape, c('shp', 'shx', 'dbf'), 
+                                  sep='.')[f.exists])), call.=FALSE)
+  } else outshape <- tempfile()
+  if (is(x, 'Raster')) {
+    require(raster)
+    writeRaster(x, {f <- tempfile(fileext='.asc')})
+    rastpath <- normalizePath(f)
+  } else if (is.character(x)) {
+    rastpath <- normalizePath(x)
+  } else stop('x must be a file path (character string), or a Raster object.')
+  if(python3==T)
+  {
+    system2('python3', args=(sprintf('"%1$s" "%2$s" -f "%3$s" "%4$s.shp"', pypath, rastpath, gdalformat,outshape)))
+  }else{
+    system2('python', args=(sprintf('"%1$s" "%2$s" -f "%3$s" "%4$s.shp"', pypath, rastpath, gdalformat,outshape)))
+  }
+  
+  shp <- sf::read_sf(paste0(outshape,".shp"))
+  
+  return(shp) 
+}
+
+
+
+
+
 #' Polygonize a classified raster layer, dropping crumbs and filling holes. 
 #'
 #' Must provide a classified raster, minimum area for which polygons should
@@ -11,7 +72,8 @@
 #' holes will be remove. 
 #' @param smooth Boolean. Should the resulting polygons geometry be simplified. 
 #' @param smooth_method String. Smoothing method to pass to \code{\link{smoothr}},
-#' smooth must be TRUE.  
+#' smooth must be TRUE.
+#' @param ... Any arguments to be passed to gdal_polgonizeR  
 #'
 #' @return Polygonized sf object representing the input raster categories. 
 #'
@@ -24,18 +86,14 @@
 #'p <- raster_to_clean_polygon(r,2500,2500,TRUE,'chaikin')
 #' }
 #' @export
-raster_to_clean_polygon<-function(r,min_area_drop_m2,max_area_fill_m2,smooth,smooth_method)
+raster_to_clean_polygon<-function(r,min_area_drop_m2,max_area_fill_m2,smooth,smooth_method,...)
 {
-  if(class(r)==class(raster()))
-  {
-    r <- stars::st_as_stars(r)
-  }
   
   max_area_fill_m2<-units::as_units(max_area_fill_m2, "m2")
   min_area_drop_m2<-units::as_units(min_area_drop_m2, "m2")
   
   # POLYGONIZE (AND DISSOLVE/MERGE BY CLASS)
-  p <- sf::st_as_sf(r,as_points = F, merge = T)
+  p <- gdal_polygonizeR(x=r,...)
   
   # RENAME FIELDS
   names(p) <- c("class","geometry")
@@ -59,3 +117,9 @@ raster_to_clean_polygon<-function(r,min_area_drop_m2,max_area_fill_m2,smooth,smo
   return(p_sm)
   
 }
+
+
+
+
+
+
